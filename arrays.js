c6e74bbad0e9881fe559936e7e1164fcb3815809
LaurenGifford <lauren.gifford.17@gmail.com>
2021-04-06 15:50:59 -0400

    array recursions complete

--- arrays.js
+++ arrays.js
@@ -3,3 +3,27 @@ function addElements(array, index) {
         return array[index] + addElements(array, index+1)
     } else {return 0}
 }
+
+function isLargest(array, compare=0) { 
+    let a = array[0]
+    let b = array[1]
+    let highest =  a > b ? a : b
+    let newCompare = highest > compare ? highest : compare
+    let newArray = array.filter(el => el !== highest)
+
+    if (array.length > 1){
+    return isLargest(newArray, newCompare)
+    } else {return newCompare}
+}
+
+
+function doesInclude(array, element) {
+    if (array[0] !== element) {
+        let newArray = array.slice(1)
+        if (newArray.length === 0) {return false}
+        else {return doesInclude(newArray, element)}
+    } else {
+        return true}
+}
+
+doesInclude([1,2,3,4,5],1)

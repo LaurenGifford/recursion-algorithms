ef3a571c163f9a473c2b1d429b2e7d9647a03e35
LaurenGifford <lauren.gifford.17@gmail.com>
2021-05-06 11:57:26 -0400

    adds pointer method example

--- arrays.js
+++ arrays.js
@@ -26,7 +26,7 @@ function doesInclude(array, element) {
         return true}
 }
 
-
+// Uses counter approach 
 function same(arr1, arr2) {
     if (arr1.length !== arr2.length) {
         return false
@@ -49,4 +49,19 @@ function same(arr1, arr2) {
         }
     }
     return true
+}
+
+// Uses pointer approach
+function sumZero(arr) {
+    let left = 0
+    let right = arr.length - 1
+
+    while (left < right) {
+        let sum  = arr[left] + arr[right]
+        if (sum === 0) {
+            return [arr[left], arr[right]]
+        } else if (sum > 0) {
+            right--
+        } else {left++}
+    }
 }

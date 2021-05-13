3c8c1f9bd06570dd96f4cc582dd7bdeda595380e
LaurenGifford <lauren.gifford.17@gmail.com>
2021-05-13 15:52:36 -0400

    example helper recursion strategy

--- recursion.js
+++ recursion.js
@@ -17,3 +17,18 @@ function factorial(num) {
     if (num === 1) return 1
     return num * factorial(num - 1)
 }
+
+// helper recursion
+function collectOdds(nums) {
+    results = []
+
+    function helper(helperInput) {
+        if (helperInput.length === 0) return
+        if (helperInput[0] % 2 !== 0) {
+            results.push(helperInput[0])
+        }
+        helper(helperInput.slice(1))
+    }
+    helper(arr)
+    return results
+}

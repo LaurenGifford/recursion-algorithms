97e9c6562fe5314eaab3f97937eb6b63afabf957
LaurenGifford <lauren.gifford.17@gmail.com>
2021-05-18 15:21:39 -0400

    adds naive string search

--- searching-algorithms/linear-binary.js
+++ searching-algorithms/linear-binary.js
@@ -22,4 +22,14 @@ function binarySearch(arr, val) {
     return arr[mid] === val ? mid : -1
 }
 
-
+// Naive String Search
+function naive(long, short) {
+    let count = 0
+    for (let i = 0; i < long.length; i++) {
+        for (let j = 0; j < short.length; j++) {
+            if (long[i+j] !== short[j]) break;
+            if (j === short.length -1) count++;
+        }
+    }
+    return count
+}

3b865b044bc029f3756d6fe4927678c226be4e70
LaurenGifford <lauren.gifford.17@gmail.com>
2021-05-19 15:08:40 -0400

    optimized bubble

--- sorting-algorithms/bubble.js
+++ sorting-algorithms/bubble.js
@@ -1,12 +1,31 @@
 // Basic example
+// function bubble(arr) {
+//     const swap = (arr, ind1, ind2) => {
+//         [arr[ind1], arr[ind2]] = [arr[ind2], arr[ind1]]
+//     }
+//     for (let i = arr.length; i > 0; i--) {
+//         for (let j = 0; j < i-1; j++) {
+//             if (arr[j] > arr[j+1]) swap(arr, i, j+1)
+//         }
+//     }
+//     return arr
+// }
+
+// optimized
 function bubble(arr) {
+    let noSwaps
     const swap = (arr, ind1, ind2) => {
         [arr[ind1], arr[ind2]] = [arr[ind2], arr[ind1]]
     }
     for (let i = arr.length; i > 0; i--) {
+        noSwaps = true
         for (let j = 0; j < i-1; j++) {
-            if (arr[j] > arr[j+1]) swap(arr, i, j+1)
+            if (arr[j] > arr[j+1]) {
+                swap(arr, i, j+1)
+                noSwaps = false
+            }
         }
+        if (noSwaps) break
     }
     return arr
 }

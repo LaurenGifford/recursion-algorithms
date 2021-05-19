ca76b5702fecbc35984382afbaffe887c3886639
LaurenGifford <lauren.gifford.17@gmail.com>
2021-05-19 15:02:30 -0400

    basic bubble sort function

---
+++ sorting-algorithms/bubble.js
@@ -0,0 +1,19 @@
+// Basic example
+function bubble(arr) {
+    const swap = (arr, ind1, ind2) => {
+        [arr[ind1], arr[ind2]] = [arr[ind2], arr[ind1]]
+    }
+    for (let i = arr.length; i > 0; i--) {
+        for (let j = 0; j < i-1; j++) {
+            if (arr[j] > arr[j+1]) swap(arr, i, j+1)
+        }
+    }
+    return arr
+}
+
+// swap
+function swap(arr, ind1, ind2) {
+    let temp = arr[ind1]
+    arr[ind1] = arr[ind2]
+    arr[ind2] = temp
+}
